af17b0d89c0e483c5ead34de3a24107c9cc23f88
Jobinjt <97350074+Jobinjt@users.noreply.github.com>
2024-05-15 11:25:36 +0530

    python
    python file is here

---
+++ example.py
@@ -0,0 +1,20 @@
+"""print("Enter your Name")
+x = input()
+print("name :",x)
+"""
+"""x=input("Enter your Name:")
+print(x)
+"""
+x=int(input("Enter first score"))
+y=int(input("Enter second score"))
+print(X+Y)
+
+course :=("RONORO ZORO")
+print(course)
+print(len(course))
+
+print(course[0])
+print(course[1])
+print(course[2])
+print(course[4])
+print(course[0:4])
